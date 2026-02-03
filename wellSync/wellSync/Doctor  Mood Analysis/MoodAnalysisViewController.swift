f441931b3c472f37cdafd47c6e00917a4e45d035
Vidit Agarwal <viditagarwal60@gmail.com>
2026-02-03 13:07:45 +0530

    Mood Analysis Doctor

---
+++ wellSync/wellSync/Doctor  Mood Analysis/MoodAnalysisViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MoodAnalysisViewController.swift
+//  wellSync
+//
+//  Created by Vidit Agarwal on 03/02/26.
+//
+
+import UIKit
+
+class MoodAnalysisViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
