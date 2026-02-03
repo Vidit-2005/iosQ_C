da6d3d23013e5964156d1cb4e63c85a963dfd6ee
Kanak136 <gkanak136@gmail.com>
2026-02-03 11:37:23 +0530

    Patient Detail Profile in Psychologist's Side is Added

---
+++ wellSync/wellSync/PatientDetailDoctorProfile/PatientProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PatientProfileViewController.swift
+//  wellSync
+//
+//  Created by GEU on 02/02/26.
+//
+
+import UIKit
+
+class PatientProfileViewController: UIViewController {
+    
+    @IBOutlet weak var PatientProfileCollectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
