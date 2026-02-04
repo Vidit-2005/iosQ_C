60642a62f29a7166899bcbd10382ccc117e6fc40
Kanak136 <gkanak136@gmail.com>
2026-02-04 15:29:47 +0530

    Added SummarizedReport Cell in Patient Detail Profile in Doctors Side

--- wellSync/wellSync/PatientDetailDoctorProfile/PatientProfileViewController.swift
+++ wellSync/wellSync/PatientDetailDoctorProfile/PatientProfileViewController.swift
@@ -13,14 +13,20 @@ class PatientProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        registerCells()
+//        let Profilelayout = createLayoutProfile()
+//        let SessionNoteslayout = createLayoutSessionNotes()
+        let Layout = generateLayout()
+        PatientProfileCollectionView.setCollectionViewLayout(Layout, animated: true)
         
-        PatientProfileCollectionView.register(UINib(nibName: "ProfileCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "ProfileCollectionViewCell")
-        let layout = createLayout()
-        PatientProfileCollectionView.setCollectionViewLayout(layout, animated: true)
         PatientProfileCollectionView.delegate = self
         PatientProfileCollectionView.dataSource = self
+    }
+    
+    func registerCells(){
+        PatientProfileCollectionView.register(UINib(nibName: "ProfileCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "ProfileCollectionViewCell")
+        PatientProfileCollectionView.register(UINib(nibName: "SessionNotesCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "SessionNotesCollectionViewCell")
+        PatientProfileCollectionView.register(UINib(nibName: "SummarizedReportCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "SummarizedReportCollectionViewCell")
     }
     
 }
@@ -31,15 +37,69 @@ extension PatientProfileViewController: UICollectionViewDelegate, UICollectionVi
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let profilecell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProfileCollectionViewCell", for: indexPath) as! ProfileCollectionViewCell
-        profilecell.configureCell()
-        return profilecell
+        if indexPath.section == 0{
+            let profilecell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProfileCollectionViewCell", for: indexPath) as! ProfileCollectionViewCell
+            profilecell.configureCell()
+            return profilecell }
+        else if indexPath.section == 1{
+            let sessionNotesCell = collectionView.dequeueReusableCell(withReuseIdentifier: "SessionNotesCollectionViewCell", for: indexPath) as! SessionNotesCollectionViewCell
+            sessionNotesCell.configureCell()
+            cardStyle(cell: sessionNotesCell)
+            return sessionNotesCell
+        }else if indexPath.section == 2{
+            let summarizedReportCell = collectionView.dequeueReusableCell(withReuseIdentifier: "SummarizedReportCollectionViewCell", for: indexPath) as! SummarizedReportCollectionViewCell
+            summarizedReportCell.configureCell()
+            cardStyle(cell: summarizedReportCell)
+            return summarizedReportCell
+        }else{
+            let summarizedReportCell = collectionView.dequeueReusableCell(withReuseIdentifier: "SummarizedReportCollectionViewCell", for: indexPath) as! SummarizedReportCollectionViewCell
+            summarizedReportCell.configureCell()
+            cardStyle(cell: summarizedReportCell)
+            return summarizedReportCell
+        }
+    }
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 3
     }
 }
 
 extension PatientProfileViewController{
+    
+    func generateLayout() -> UICollectionViewLayout {
 
-    func createLayout() -> UICollectionViewLayout{
+           let layout = UICollectionViewCompositionalLayout { [weak self]
+               sectionIndex, environment -> NSCollectionLayoutSection in
+
+               guard let self = self else {
+                   return self!.generateSectionForCells()
+               }
+
+               switch sectionIndex {
+               case 0:
+                   return self.generateSectionForProfile()
+               case 1:
+                   return self.generateSectionForCells()
+               case 2:
+                   return self.generateSectionForCells()
+               default:
+                   return self.generateSectionForCells()
+               }
+           }
+
+           return layout
+       }
+    func cardStyle(cell:UICollectionViewCell){
+        cell.backgroundColor = .systemBackground
+        cell.layer.cornerRadius = 16
+        cell.layer.masksToBounds = false
+        cell.layer.borderWidth = 0.5
+        cell.layer.borderColor = UIColor.separator.cgColor
+        cell.layer.shadowColor = UIColor.black.cgColor
+        cell.layer.shadowOpacity = 0.08
+        cell.layer.shadowRadius = 10
+        cell.layer.shadowOffset = CGSize(width: 0, height: 4)
+    }
+    func generateSectionForProfile() -> NSCollectionLayoutSection{
             let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(180))
             let item = NSCollectionLayoutItem(layoutSize: itemSize)
             
@@ -49,7 +109,17 @@ extension PatientProfileViewController{
             let section = NSCollectionLayoutSection(group: group)
             
             section.contentInsets = NSDirectionalEdgeInsets(top: 12, leading: 16, bottom: 12, trailing: 16)
-        let layout = UICollectionViewCompositionalLayout(section: section)
-        return layout
+        return section
+    }
+    
+    func generateSectionForCells() -> NSCollectionLayoutSection{
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(150))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(150))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 1)
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 12, leading: 16, bottom: 12, trailing: 16)
+        return section
     }
 }
--- wellSync/wellSync/PatientDetailDoctorProfile/SessionNotesCollectionViewCell.swift
+++ wellSync/wellSync/PatientDetailDoctorProfile/SessionNotesCollectionViewCell.swift
@@ -9,9 +9,22 @@ import UIKit
 
 class SessionNotesCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet var SessionNoteLabel: UILabel!
+    @IBOutlet var notesLabel1: UILabel!
+    @IBOutlet var notesLabel2: UILabel!
+    @IBOutlet var notesLabel3: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
     }
+    
+    
 
+    func configureCell(){
+        SessionNoteLabel.text = "Session Notes"
+        notesLabel1.text = "Feeling depressive on Sunday evenings. I'm not sure if it's the work or the loneliness."
+        notesLabel2.text = "Feeling Lonely and anxious. And not interested in social activities."
+        notesLabel3.text = "Want to escape from People Interaction."
+    }
 }
